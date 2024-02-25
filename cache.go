8f33393a66482ff4884bbc335206a16aac680957
brianvoe <brian@webiswhatido.com>
2024-02-25 11:42:18 -0600

    cache - talk about what i would like to add

--- cache.go
+++ cache.go
@@ -1,4 +1,6 @@
 package gofindit
 
+// TODO: Implement a cache that based upon the search query, generate an id based upon the search query
+// and store the results in a map[string]any
 type Cache struct {
 }
